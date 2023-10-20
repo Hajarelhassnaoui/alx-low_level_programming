b003b76ef8e1bca945772ae078714366bc533490
hajarelhassnaoui <elhassnaouihajarr@gmail.com>
2023-10-20 15:29:13 -0700

    g

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -p /tmp https://github.com/Hajarelhassnaoui/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
-export LD_PRELAOD=/tmp/libgiga.so
+wget -P /tmp https://github.com/Tolulope05/alx-low_level_programming/raw/master/0x18-dynamic_libraries/nrandom.so
+export sD_PRELOAD=/tmp/nrandom.so
